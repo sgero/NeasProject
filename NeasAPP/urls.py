592d35639cdf9ca02a141156e4552d30dc835cf6
SebasG <sgarciaguerrero@safareyes.es>
2023-04-15 20:35:04 +0200

    Añadidos tutoriales para LOGIN y REGISTRO en Django (Accounts)

--- NeasAPP/urls.py
+++ NeasAPP/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', inicio),
+    path('inicio', inicio),
     path('ruta/crear/', crear_ruta),
     path('ruta/', mostrar_rutas),
     path('login', registrar_usuario),
