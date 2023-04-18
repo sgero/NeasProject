33ff57c3c4be2ebe90dc9344f770a7d68820a7df
SebasG <sgarciaguerrero@safareyes.es>
2023-04-18 16:32:09 +0200

    Password Nuevo

--- NeasAPP/forms.py
+++ NeasAPP/forms.py
@@ -8,9 +8,9 @@ class FormularioRegistro(UserCreationForm):
                              help_text='Requerido. Ingrese una dirección de correo electrónico válida.')
     username = forms.CharField(max_length=255, required=True,
                                help_text='Requerido. 255 caracteres o menos. Solo letras, números y @/./+/-/_ caracteres.')
-    password = forms.PasswordInput()
-    password_confirmation = forms.PasswordInput()
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput()
 
     class Meta:
         model = UsuarioLogin
-        fields = ('email', 'username', 'password', 'password_confirmation')
+        fields = ('email', 'username', 'password1', 'password2')
