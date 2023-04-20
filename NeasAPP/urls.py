ec28d58aa31189c05518f1f6887ee817151699b2
SebasG <sgarciaguerrero@safareyes.es>
2023-04-20 12:12:08 +0200

    Arreglado URLS

--- NeasAPP/urls.py
+++ NeasAPP/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('ruta/crear/', crear_ruta),
-    path('ruta/', mostrar_ruta),
+    path('ruta/crear/', crear_ruta, name='crear_ruta'),
+    path('ruta/', mostrar_ruta, name='mostrar_ruta'),
     path('login/usuario', login_usuario, name='login_usuario'),
     path('login/operador', login_operador, name='login_operador'),
     path('registrar/usuario/', registrar_usuario, name='registrar_usuario'),
