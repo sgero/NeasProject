40cfd992957dc6268229bebeb7f69a9a5c30c4cb
Sebastian <sgarciaguerrero@safareyes.es>
2023-05-19 13:50:12 +0200

    Barra lateral + DarkMode

--- NeasAPP/static/dark-mode.js
+++ NeasAPP/static/dark-mode.js
@@ -21,4 +21,16 @@
 //   toggleDarkMode();
 // }
 
+const body = document.querySelector('body');
+      sidebar = body.querySelector('.sidebar');
+      toggle = body.querySelector('.toggle');
+      searchBtn = body.querySelector('.search-box .bx-search');
+      modeSwitch = body.querySelector('.toggle-switch');
+      modeText = body.querySelector('.mode-text');
+
+      modeSwitch.addEventListener('click', () => {
+          body.classList.toggle('dark');
+      });
+
+
 
